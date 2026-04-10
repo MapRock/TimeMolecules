271f40f7e675e42af4e9fa03d26a0f3cceff0de5
MapRock <32338305+MapRock@users.noreply.github.com>
2026-04-10 11:07:41 -0600

    Add SQL query for fact table example

---
+++ tutorials/star_schema/fact_table_example.sql
@@ -0,0 +1,17 @@
+SELECT
+	e.[EventID],
+	e.EventDate,
+	e.CaseID,
+	CASE WHEN l.PropertyValueNumeric IS NULL THEN -1 ELSE l.PropertyValueNumeric END AS [LocationID],
+	f.PropertyValueNumeric AS [Fuel],
+	w.PropertyValueNumeric [Weight]
+FROM
+	[EventsFact] e (NOLOCK)
+	LEFT JOIN [dbo].[EventPropertiesParsed] f (NOLOCK) ON f.EventID=e.EventID AND f.PropertyName='Fuel'
+	LEFT JOIN [dbo].[EventPropertiesParsed] w (NOLOCK) ON w.EventID=e.EventID AND w.PropertyName='Weight'
+	LEFT JOIN [dbo].[CasePropertiesParsed] l (NOLOCK) ON l.CaseID=e.CaseID AND l.PropertyName='LocationID'
+WHERE
+	(
+		f.PropertyValueNumeric IS NOT NULL OR
+		w.PropertyValueNumeric IS NOT NULL
+	)
